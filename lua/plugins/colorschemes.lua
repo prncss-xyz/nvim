0a9f9943a154cb9ff4d3972b6a9c4078f218d5f5
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2025-05-04 22:00:39 -0400

    feat: colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,14 +2,17 @@ local not_vscode = require("my.conds").not_vscode
 local personal = require("my.conds").personal
 local tui = require("my.conds").tui
 
-local function active(name, flag)
+local current = "gruvbox"
+local background = "dark"
+
+local function colorscheme(name)
 	return function(config)
 		config.cond = not_vscode
-		if flag or flag == nil then
+		if name == current then
 			config.priority = 1000
 			config.lazy = false
 			function config.config()
-				vim.o.background = "dark"
+				vim.o.background = background
 				vim.cmd.colorscheme(name)
 			end
 		end
@@ -18,30 +21,27 @@ local function active(name, flag)
 end
 
 return {
-	active("matrix")({
-		"iruzo/matrix-nvim",
-	}),
-	{
-		"calind/selenized.nvim",
-		commit = "a43e34d",
-	},
-	{
-		"rebelot/kanagawa.nvim",
-	},
-	{
-		"rose-pine/neovim",
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-	},
-	{
-		"ishan9299/nvim-solarized-lua",
-		commit = "d69a263",
-	},
-	{
+	colorscheme("matrix")({
 		"iruzo/matrix-nvim",
 		commit = "5fafe6b",
-	},
+	}),
+	colorscheme("selenized")({
+		"calind/selenized.nvim",
+		commit = "a43e34d",
+	}),
+	colorscheme("kanagawa")({
+		"rebelot/kanagawa.nvim",
+	}),
+	colorscheme("rose-pine")({
+		"rose-pine/neovim",
+	}),
+	colorscheme("gruvbox")({
+		"ellisonleao/gruvbox.nvim",
+	}),
+	colorscheme("solarized")({
+		"ishan9299/nvim-solarized-lua",
+		commit = "d69a263",
+	}),
 	{
 		"sphamba/smear-cursor.nvim",
 		opts = false and {
