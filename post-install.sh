bb29cec422ed955afda97b9097c55d9563d173e6
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2025-12-13 11:57:44 -0500

    update post-install

--- post-install.sh
+++ post-install.sh
@@ -5,4 +5,4 @@ if [ "$ID" == "arch" ]; then
   sudo pacman --needed -Sy base-devel fd go jre21-openjdk-headless neovim npm ripgrep rust yazi
 fi
 # Install Mason packages I use the most
-nvim --headless -c 'MasonInstall eslint-lsp json-lsp prettierd ltex-ls lua-language-server marksman stylua vtsls yaml-language-server' -c qall
+nvim --headless -c 'MasonInstall json-lsp prettierd ltex-ls-plus lua-language-server marksman stylua vtsls yaml-language-server' -c qall
