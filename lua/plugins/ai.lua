77edc78674107e6f665957a73a118669f4c958e3
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-04-05 16:40:17 -0400

    changes from crotte on Sun Apr 5 16:40:17 EDT 2026

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,117 +1,15 @@
 local not_vscode = require("my.conds").not_vscode
 local personal = require("my.conds").personal
 local domain = require("my.parameters").domain
-local pick = domain.pick
 local ai = domain.ai
 local ai_insert = require("my.parameters").ai_insert
-local reverse = require("my.parameters").reverse
 
 -- TODO: augmentcode
 local completion = personal("copilot", "copilot") -- "copilot" | "windsurf" |  "none"
-local chat = personal("none", "sidekick") -- 'sidekick' | 'avante' | 'copilotchat' | 'claude' | 'agentic' | 'none'
+local chat = personal("agentic", "sidekick") -- 'sidekick' | 'avante' | 'copilotchat' | 'claude' | 'agentic' | 'none'
 local sidekick_chat = personal(function() end, "claude") -- "opencode" | "claude" | "gemini"
 
 return {
-	{
-		"yetone/avante.nvim",
-		version = false,
-		build = "make",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			"nvim-tree/nvim-web-devicons",
-		},
-		opts = {
-			provider = "gemini",
-			providers = {
-				claude = {
-					api_key_name = "cmd:pass show anthropic.com/juliette.lamarche.xyz@gmail.com/keys/nvim",
-				},
-				openai = {
-					api_key_name = "cmd:pass show openai.com/juliette.lamarche.xyz@gmail.com/keys/nvim",
-				},
-				gemini = {
-					api_key_name = "cmd:pass show google.com/juliette.lamarche.xyz@gmail.com/keys/nvim",
-					endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-					-- find new models here: https://aistudio.google.com/prompts/new_chat
-					-- model = "gemini-2.0-pro-exp-02-05",
-					-- model = "gemini-2.5-pro-preview-03-25",
-					model = "gemini-2.5-flash-preview-04-17",
-					timeout = 30000, -- Timeout in milliseconds
-					temperature = 0,
-					max_tokens = 4096,
-				},
-			},
-			web_search_engine = {
-				provider = "tavily", -- tavily, serpapi, searchapi, google, kagi, brave, or searxng
-				providers = {
-					tavily = {
-						api_key_name = "cmd:pass show tavily.com/juliette.lamarche.xyz@gmail.com/keys/nvim",
-					},
-				},
-			},
-			mappings = {
-				ask = ai .. "a",
-				edit = ai .. "e",
-				refresh = ai .. "r",
-			},
-		},
-		keys = {
-			{
-				ai .. "a",
-				function()
-					require("avante.api").ask()
-				end,
-				mode = { "n", "x" },
-				desc = "Avante Ask",
-			},
-			{
-				ai .. "e",
-				function()
-					require("avante.api").edit()
-				end,
-				mode = { "n", "x" },
-				desc = "Avante Edit",
-			},
-			{ ai .. "c", "<cmd>AvanteChat<cr>", desc = "Avante Chat" },
-			{ ai .. reverse("c"), "<cmd>AvanteChatNew<cr>", desc = "Avante Chat New" },
-			{
-				ai .. pick,
-				function()
-					require("avante.api").select_history()
-				end,
-				desc = "Avante Pick Chat",
-			},
-			{
-				ai .. "x",
-				function()
-					require("avante.api").stop()
-				end,
-				desc = "Avante Stop",
-			},
-			{
-				ai .. "s",
-				function()
-					require("avante.api").get_suggestion()
-				end,
-				desc = "Avante Suggest",
-			},
-		},
-		cmd = {
-			"AvanteModels",
-			"AvanteAsk",
-			"AvanteChat",
-			"AvanteToggle",
-			"AvanteEdit",
-			"AvanteRefresh",
-			"AvanteBuild",
-			"AvanteSwitchProvider",
-			"AvanteClear",
-		},
-		enabled = chat == "avante",
-		cond = not_vscode,
-	},
 	{
 		"Exafunction/windsurf.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
@@ -208,20 +106,17 @@ return {
 	},
 	{
 		"carlos-algms/agentic.nvim",
-		opts = {
-			provider = "opencode-acp", -- "gemini-cli" | "opencode"
-		},
+		opts = { provider = "gemini-acp" },
 		keys = {
 			{
-				ai_insert.toggle,
+				"okk",
 				function()
 					require("agentic").toggle()
 				end,
-				mode = { "n", "v", "i" },
 				desc = "Toggle Agentic Chat",
 			},
 			{
-				ai .. "i",
+				"ok" .. "i",
 				function()
 					require("agentic").add_selection_or_file_to_context()
 				end,
@@ -229,20 +124,18 @@ return {
 				desc = "Add file or selection to Agentic to Context",
 			},
 			{
-				ai .. "a",
+				"ok" .. "a",
 				function()
 					require("agentic").new_session()
 				end,
-				mode = { "n", "v", "i" },
 				desc = "New Agentic Session",
 			},
 			{
-				ai .. "r",
+				"ok" .. "r",
 				function()
 					require("agentic").restore_session()
 				end,
 				desc = "Agentic Restore session",
-				silent = true,
 				mode = { "n", "v", "i" },
 			},
 		},
