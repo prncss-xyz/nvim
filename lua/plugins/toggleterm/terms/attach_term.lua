134cca3356d48d46ac0adc3f27bb2c1559bacf92
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-07-21 21:21:25 -0400

    changes from crotte on Tue Jul 21 21:21:25 EDT 2026

--- lua/plugins/toggleterm/terms/attach_term.lua
+++ lua/plugins/toggleterm/terms/attach_term.lua
@@ -3,6 +3,19 @@ local M = {}
 local window = require("plugins.toggleterm.terms.window")
 local is_in_view = window.is_in_view
 
+local function get_local_url(line)
+	return false
+		or line:match("https?://[%w%-%.]*localhost[:/?#][^%s]*")
+		or line:match("https?://[%w%-%.]*localhost%s")
+		or line:match("https?://[%w%-%.]*localhost$")
+		or line:match("https?://localhost[:/?#][^%s]*")
+		or line:match("https?://localhost%s")
+		or line:match("https?://localhost$")
+		or line:match("https?://127%.0%.0%.1[:/?#][^%s]*")
+		or line:match("https?://127%.0%.0%.1%s")
+		or line:match("https?://127%.0%.0%.1$")
+end
+
 function M.attach_term(term, send)
 	if not term.bufnr or not vim.api.nvim_buf_is_valid(term.bufnr) then
 		return
@@ -35,7 +48,7 @@ function M.attach_term(term, send)
 			if not url_sent then
 				local lines = vim.api.nvim_buf_get_lines(bufnr, first_line, new_last_line, false)
 				for _, line in ipairs(lines) do
-					local url = line:match("https?://%S+")
+					local url = get_local_url(line)
 					if url then
 						url_sent = true
 						send({ type = "url", value = url })
