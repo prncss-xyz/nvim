53c0b2376ef0a4cc4469825b92867de57fdfba9a
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-08-16 18:57:59 -0400

    changes from LAPTOP-D94IIJIV on Sun Aug 16 18:57:59 EDT 2026

--- lua/options.lua
+++ lua/options.lua
@@ -81,23 +81,9 @@ deep_merge(vim, {
 		neovide_opacity = 0.85,
 		mapleader = " ",
 		maplocalleader = ",",
+		clipboard = "unnamedplus",
 	},
 })
-
-if vim.fn.has("wsl") == 1 then
-	vim.g.clipboard = {
-		name = "WslClipboard",
-		copy = {
-			["+"] = "clip.exe",
-			["*"] = "clip.exe",
-		},
-		paste = {
-			["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-			["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-		},
-		cache_enabled = 0,
-	}
-end
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "markdown",
